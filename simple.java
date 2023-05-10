805eb72b73e866fe6953b525f605dda4d517d759
kanchaka-manupriya <129038539+kanchaka-manupriya@users.noreply.github.com>
2023-05-11 02:07:29 +0530

    Update simple.java

--- simple.java
+++ simple.java
@@ -1,6 +1,6 @@
 public class simple
 {
 		public static void main(String[] args) {
-					System.out.println("Hello World! shakaboom shaka shaka bomm");
+					System.out.println("Hello World! hastala vista");
 						}
 }
