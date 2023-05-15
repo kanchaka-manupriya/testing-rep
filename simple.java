734fc52fe09f0e843afdb8e846a79b91fbd5f640
manupriya <kanchaka77@gmail.com>
2023-05-15 23:32:24 +0530

    trying ssh1

--- simple.java
+++ simple.java
@@ -1,6 +1,6 @@
 public class simple
 {
 		public static void main(String[] args) {
-					System.out.println("Hello World! hastala vista intergrated version- edt using vm");
+					System.out.println("Hello World! hastala vista intergrated version- edt using vm- ssh trying1");
 						}
 }
